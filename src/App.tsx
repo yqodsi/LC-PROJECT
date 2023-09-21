e8cca9ae26fc3e87fafabdbeea9622567a171627
yahyaqodsiv12 <yahya@v12software.com>
2023-09-21 16:30:54 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setIsHeaderVisible(false); // Hide the header when scrolled down 100 pixels
-    } else {
-      setIsHeaderVisible(true); // Show the header when scrolled up
-    }
-  };
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+  // const [count, setCount] = useState(0)
+  // const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  // const handleScroll = () => {
+  //   if (window.scrollY > 100) {
+  //     setIsHeaderVisible(false); // Hide the header when scrolled down 100 pixels
+  //   } else {
+  //     setIsHeaderVisible(true); // Show the header when scrolled up
+  //   }
+  // };
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll);
   
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
     
   return (
     <>
-<div className="navbar bg-white text-black  fixed z-10" style={{opacity: "0.4"}}>
-  <a className="btn btn-ghost normal-case text-xl">Walid's project</a>
+<div className="navbar bg-white text-black sticky z-10" style={{opacity: "0.4", width: "90%", justifyContent: "center", display: "flex"}}>
+  <a className="btn btn-ghost normal-case text-xl"></a>
 </div>
+      {/* <img src="/IMMSITO/Casa/pexels-vecislavas-popa-1571463.jpg" alt="" /> */}
 <div className="img-accordion">
-    <div className="accordion-item">
-      {/* <img src="/IMMSITO/Elettricita/switch-cabinet-2147370.jpg" alt="" /> */}
-      <img src="/IMMSITO/Casa/pexels-vecislavas-popa-1571463.jpg" alt="" />
-      <p>PROGETTAZIONE DI IMPIANTI ELETTRICI RESIDENZIALI</p>
-    </div>
+<div className="accordion-item">
+
+<img src="/IMMSITO/Casa/pexels-vecislavas-popa-1571463.jpg" alt="" />
+
+ <p className='flex flex-col'> <img style={{width: "50px"}} src="/Logo LC.png" alt="" /> <span className='main_title'>Residenziale</span><div>PROGETTAZIONE DI IMPIANTI ELETTRICI RESIDENZIALI</div></p>
+</div>
     <div className="accordion-item">
 
            <img src="/IMMSITO/Capannone/pexels-pixabay-256297.jpg" alt="" />
 
-            <p>PROGETTAZIONE DI IMPIANTI ELETTRICI <br/> INDUSTRIALI/COMMERCIALI</p>
+            <p className='flex flex-col'> <img style={{width: "50px"}} src="/Logo LC.png" alt="" /> <span className='main_title'>Residenziale</span><div>PROGETTAZIONE DI IMPIANTI ELETTRICI <br/> INDUSTRIALI/COMMERCIALI</div></p>
         </div>
                 <div className=" accordion-item">
       <img src="/IMMSITO/Fotovoltaico/pexels-kelly-4320480.jpg" alt="" />
