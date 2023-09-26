f073b5fdf62a606c29d289516402009edf42f8a6
yahyaqodsiv12 <yahya@v12software.com>
2023-09-26 16:58:05 +0100

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
           secondary: "#f6d860", // This is the color of the secondary button
           accent: "#146C94", // This is the color of the accent button
           neutral: "#3d4451", // This is the color of the neutral button
-          "base-100": "#a991f7", // This is the color of the background
+          "base-100": "#ffff", // This is the color of the background
           "--rounded-box": "1rem", // This sets the border radius for large boxes
           "--rounded-btn": "0.5rem", // This sets the border radius for buttons
           "--rounded-badge": "1.9rem", // This sets the border radius for badges
