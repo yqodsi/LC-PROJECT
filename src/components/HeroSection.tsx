74ad7548153003c2f425ea5344bad5a37a0f2e09
yahyaqodsiv12 <yahya@v12software.com>
2023-09-28 15:48:33 +0100

    WIP

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -169,9 +169,9 @@ function HeroSection() {
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-full bg-blue-600 px-5 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get started
+                  Scopri
                 </a>
               </div>
             </div>
